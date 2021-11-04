e59180b69c79e0b9a037df1fe508cfcc9b08cb4e
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-11-04 14:17:23 +0600

    Insert Data Into Database

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(cors());
+app.use(express.json());
 
 // Database URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6nwy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -22,11 +23,24 @@ async function run() {
         await client.connect();
         const database = client.db("planetParadise");
         const tourCollection = database.collection("tourPackages");
+
+        // POST API
+        app.post('/tourpackages', async (req, res) => {
+            const newTourPack = req.body;
+
+            const result = await tourCollection.insertOne(newTourPack);
+            console.log(result);
+            res.json(result);
+        });
+
     } finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
+
+
+
 
 // Request and Response
 app.get('/', (req, res) => {
